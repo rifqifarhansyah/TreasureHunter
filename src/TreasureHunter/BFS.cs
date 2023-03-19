7d94ea3b81537d7bf3116ff5fc408dbc1a0f5f43
Zaki <zakiamanullah215@gmail.com>
2023-03-19 14:26:39 +0700

    bfs mentog

--- src/TreasureHunter/BFS.cs
+++ src/TreasureHunter/BFS.cs
@@ -64,24 +64,61 @@ namespace TreasureHunterAlgo
         public void doAction()
         {
             // Cell priority {up, right, down, left}
-            while (this.liveNode.Count != 0)
+            while (this.curNode.TreasureFound != this.m.TreasureCount)
             {
-                Node tempNode = liveNode.Dequeue();
-                if (!isDiscovered(tempNode, this.discovered))
+                if (this.liveNode.Count != 0)
                 {
-                    this.curNode = tempNode;
-                    this.discovered.Add(tempNode);
-                    if (this.m.Content[curNode.I][curNode.J] == "T")
+                    Node tempNode = liveNode.Dequeue();
+                    if (!isDiscovered(tempNode, this.discovered))
                     {
-                        this.curNode.TreasureFound++;
+                        this.curNode = tempNode;
+                        this.discovered.Add(tempNode);
+                        if (this.m.Content[curNode.I][curNode.J] == "T")
+                        {
+                            this.curNode.TreasureFound++;
+                        }
+                        if (this.m.isIdxEff(curNode.I - 1, curNode.J))
+                        {
+                            if (this.m.Content[curNode.I - 1][curNode.J] != "X" && !curNode.hasInPath(curNode.I - 1, curNode.J))
+                            {
+                                Node upperNode = new Node(curNode.I - 1, curNode.J, curNode);
+                                liveNode.Enqueue(upperNode);
+                            }
+                        }
+                        if (this.m.isIdxEff(curNode.I, curNode.J + 1))
+                        {
+                            if (this.m.Content[curNode.I][curNode.J + 1] != "X" && !curNode.hasInPath(curNode.I, curNode.J + 1))
+                            {
+                                Node upperNode = new Node(curNode.I, curNode.J + 1, curNode);
+                                liveNode.Enqueue(upperNode);
+                            }
+                        }
+                        if (this.m.isIdxEff(curNode.I + 1, curNode.J))
+                        {
+                            if (this.m.Content[curNode.I + 1][curNode.J] != "X" && !curNode.hasInPath(curNode.I + 1, curNode.J))
+                            {
+                                Node upperNode = new Node(curNode.I + 1, curNode.J, curNode);
+                                liveNode.Enqueue(upperNode);
+                            }
+                        }
+                        if (this.m.isIdxEff(curNode.I, curNode.J - 1))
+                        {
+                            if (this.m.Content[curNode.I][curNode.J - 1] != "X" && !curNode.hasInPath(curNode.I, curNode.J - 1))
+                            {
+                                Node upperNode = new Node(curNode.I, curNode.J - 1, curNode);
+                                liveNode.Enqueue(upperNode);
+                            }
+                        }
                     }
-                    if (this.curNode.TreasureFound == this.m.TreasureCount)
-                    {
-                        break;
-                    }
+                }
+                else
+                {
+                    curNode = new Node(curNode.I, curNode.J);
+                    discovered = new List<Node> { curNode };
+                    liveNode = new Queue<Node>();
                     if (this.m.isIdxEff(curNode.I - 1, curNode.J))
                     {
-                        if (this.m.Content[curNode.I - 1][curNode.J] != "X" && !curNode.hasInPath(curNode.I - 1, curNode.J))
+                        if (this.m.Content[curNode.I - 1][curNode.J] != "X")
                         {
                             Node upperNode = new Node(curNode.I - 1, curNode.J, curNode);
                             liveNode.Enqueue(upperNode);
@@ -89,7 +126,7 @@ namespace TreasureHunterAlgo
                     }
                     if (this.m.isIdxEff(curNode.I, curNode.J + 1))
                     {
-                        if (this.m.Content[curNode.I][curNode.J + 1] != "X" && !curNode.hasInPath(curNode.I, curNode.J + 1))
+                        if (this.m.Content[curNode.I][curNode.J + 1] != "X")
                         {
                             Node upperNode = new Node(curNode.I, curNode.J + 1, curNode);
                             liveNode.Enqueue(upperNode);
@@ -97,7 +134,7 @@ namespace TreasureHunterAlgo
                     }
                     if (this.m.isIdxEff(curNode.I + 1, curNode.J))
                     {
-                        if (this.m.Content[curNode.I + 1][curNode.J] != "X" && !curNode.hasInPath(curNode.I + 1, curNode.J))
+                        if (this.m.Content[curNode.I + 1][curNode.J] != "X")
                         {
                             Node upperNode = new Node(curNode.I + 1, curNode.J, curNode);
                             liveNode.Enqueue(upperNode);
@@ -105,7 +142,7 @@ namespace TreasureHunterAlgo
                     }
                     if (this.m.isIdxEff(curNode.I, curNode.J - 1))
                     {
-                        if (this.m.Content[curNode.I][curNode.J - 1] != "X" && !curNode.hasInPath(curNode.I, curNode.J - 1))
+                        if (this.m.Content[curNode.I][curNode.J - 1] != "X")
                         {
                             Node upperNode = new Node(curNode.I, curNode.J - 1, curNode);
                             liveNode.Enqueue(upperNode);
